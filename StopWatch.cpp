18da94a5bbc7f91113fb1918ad3a194e4f1a5c00
1Manisha1 <105492210+1Manisha1@users.noreply.github.com>
2022-05-16 10:43:11 +0530

    Update StopWatch.cpp

--- StopWatch.cpp
+++ StopWatch.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <unistd.h> 
 using namespacestd;
-void displayTime(int hours, int minutes, int seconds) {
+void displayTime(int hours, int minutes, int seconds, int milliseconds) {
    cout << hours << ":"
         << minutes << ":"
         << seconds << endl;
@@ -11,6 +11,7 @@ int main() {
   int hour = 0;
   int min = 0;
   int sec = 0;
+  int millisec =0;
   while(true) {
     sleep(1);
       millisec++;
@@ -21,15 +22,17 @@ int main() {
     }
  if(sec > 59) {
       min++;
+      millisec = 0;
       sec = 0;
     } 
 
     if(min > 59) {
       hour++;
       sec = 0;
+      millisec =0;
       min = 0;
     }
 
-    displayTime(hour, min, sec);
+    displayTime(hour, min, sec, millisec);
   }
 }
